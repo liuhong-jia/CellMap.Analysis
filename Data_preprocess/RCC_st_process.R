17eee343b0cd8bd194b7e380cdf0316a8058756c
liuhong-jia <115637576+liuhong-jia@users.noreply.github.com>
2024-12-06 22:19:10 +0800

    Add files via upload

---
+++ Data_preprocess/RCC_st_process.R
@@ -0,0 +1,30 @@
+####RCC spatial transcriptomics data processing
+
+expr.data <- Seurat::Read10X_h5(filename ="GSM5924052_frozen_c_3_filtered_feature_bc_matrix.h5")
+obj <- Seurat::CreateSeuratObject(counts = expr.data, project = 'RCC', assay = 'Spatial')
+obj$slice <- 1
+obj$region <- 'BRCA'
+
+#Load the image data
+
+img <- Seurat::Read10X_Image(image.dir = 'spatial')
+Seurat::DefaultAssay(object = img) <- 'Spatial'
+img <- img[colnames(x = obj)]
+obj[['image']] <- img
+
+TLS_data <- read.csv("TLS_annotation.csv")
+head(st.obj@meta.data)
+st.obj$TLS <- TLS_data$TLS_2_cat
+
+
+###Standard, dimensionality reduction and clustering
+
+obj <- SCTransform(obj, assay = "Spatial", return.only.var.genes = FALSE, verbose = FALSE)
+obj <- RunPCA(obj, assay = "SCT", verbose = FALSE)
+obj <- FindNeighbors(obj, reduction = "pca", dims = 1:30)
+obj <- FindClusters(obj, verbose = FALSE)
+obj <- RunUMAP(obj, reduction = "pca", dims = 1:30)
+
+
+
+saveRDS(obj,file = "st.obj.rds")
